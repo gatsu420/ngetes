b4cd78d359f303f8f3e05158910652ae9166b6a9
Ranggalawe Istifajar <ranggalawe.istifajar@wartek.belajar.id>
2024-11-26 21:58:55 +0700

    Tidy scope of symbols in auth

--- auth/auth.go
+++ auth/auth.go
@@ -3,11 +3,11 @@ package auth
 import "github.com/go-chi/jwtauth/v5"
 
 type authStore struct {
-	JWTAuth *jwtauth.JWTAuth
+	jwtAuth *jwtauth.JWTAuth
 }
 
 func NewAuthStore(jwtauth *jwtauth.JWTAuth) *authStore {
 	return &authStore{
-		JWTAuth: jwtauth,
+		jwtAuth: jwtauth,
 	}
 }
