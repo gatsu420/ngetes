22df6182480807c9500a440d604cfc38534e8386
Ranggalawe Istifajar <ranggalawe.istifajar@wartek.belajar.id>
2024-12-15 14:02:04 +0700

    Implement logger at workers package

--- workers/uptime.go
+++ workers/uptime.go
@@ -1,6 +1,10 @@
 package workers
 
-import "github.com/gatsu420/ngetes/models"
+import (
+	"github.com/gatsu420/ngetes/logger"
+	"github.com/gatsu420/ngetes/models"
+	"go.uber.org/zap"
+)
 
 type UptimeOperations interface {
 	CreateUptime(u *models.Uptime) error
@@ -20,6 +24,7 @@ func (w *UptimeWorkers) CreateUptimeWorker() error {
 	uptime := &models.Uptime{}
 	err := w.Operations.CreateUptime(uptime)
 	if err != nil {
+		logger.Logger.Fatal("failed to generate uptime record", zap.Error(err))
 		return err
 	}
 
